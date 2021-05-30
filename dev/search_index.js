0459b0b991ef0bb162f4c5562bbc49da67a25d5f
Documenter.jl <documenter@juliadocs.github.io>
2021-05-30 19:19:10 +0000

    build based on 8452698

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StatGeochemBase","category":"page"},{"location":"#StatGeochemBase","page":"Home","title":"StatGeochemBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StatGeochemBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StatGeochemBase]","category":"page"}]
+}
