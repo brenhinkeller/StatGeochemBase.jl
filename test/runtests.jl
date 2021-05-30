9dd819898c1f708b689f206c33cc012072cd9f34
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-05-30 18:55:43 -0400

    Add tests for images and geochron

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@ using StatGeochemBase
 using Test
 
 @testset "Math" begin include("testMath.jl") end
+@testset "Images" begin include("testImages.jl") end
 @testset "ArrayStats" begin include("testArrayStats.jl") end
+@testset "Geochronology" begin include("testGeochronology.jl") end
