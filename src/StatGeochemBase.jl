61be2c0060e8d6a43ca7a7f3e6791a07f8f9186d
C.Brenhin Keller <cbkeller@dartmouth.edu>
2021-05-30 18:18:12 -0400

    Add Math.jl

--- src/StatGeochemBase.jl
+++ src/StatGeochemBase.jl
@@ -3,8 +3,19 @@ module StatGeochemBase
     using Reexport
     @reexport using NaNStatistics
 
+    # AVX vectorziation tools
     using LoopVectorization
+    using VectorizationBase
+    using VectorizationBase: Vec
+
+    using SpecialFunctions
+    import SpecialFunctions.erf
+    erf(x::Vec) = VectorizationBase.verf(x)
+
+    # Other dependencies
     using Interpolations
+
+    include("Math.jl")
     include("ArrayStats.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using StatGeochemBase
 using Test
 
+@testset "Math" begin include("testMath.jl") end
 @testset "ArrayStats" begin include("testArrayStats.jl") end
