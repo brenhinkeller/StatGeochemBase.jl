626b878cb2cda6abb116a293dade4ca2c2081024
C. Brenhin Keller <cbkeller@dartmouth.edu>
2022-11-14 23:42:47 -0500

    Add more tests for `linterp1s!`

--- test/testInterpolations.jl
+++ test/testInterpolations.jl
@@ -26,10 +26,17 @@
     xq = 5:0.5:6
     @test linterp1!(similar(xq), 1:10, 21:30, xq) == [25.0, 25.5, 26.0]
     xq = 5:0.5:6
+
     @test linterp1s!(similar(xq), 1:10, 21:30, xq) == [25.0, 25.5, 26.0]
     @test linterp1s!(similar(xq), collect(10:-1:1), collect(21:30), xq) == [26.0, 25.5, 25.0]
     xq = 5:0.5:7
     @test linterp1s!(similar(xq), collect(10:-1:1), collect(21:30), xq; knot_index=ones(Int,5), sort_index=ones(Int,10)) == [26.0, 25.5, 25.0, 24.5, 24]
+    xq = 0:0.01:1
+    x = rand(200)
+    y = rand(200)
+    yq = linterp1s(x, y, xq)
+    @test linterp1s!(similar(xq), x, y, xq) ≈ yq
+
     xq = 0:11
     @test linterp1!(similar(xq, Float64), 1:10, 1:10, xq, extrapolate=:Linear) == 0:11
     xq = 0.5:10.5
