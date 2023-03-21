b434464f5608b825dae2534e20dbbc8e07b6c1ad
C. Brenhin Keller <cbkeller@dartmouth.edu>
2023-03-21 17:53:16 -0400

    Update linreg with sligtly faster version from Isoplot.jl

--- src/Math.jl
+++ src/Math.jl
@@ -531,11 +531,29 @@
     ```julia
     (a,b) = linreg(x::AbstractVector, y::AbstractVector)
     ```
-
     Returns the coefficients for a simple linear least-squares regression of
     the form `y = a + bx`
+
+    ### Examples
+    ```
+    julia> a, b = linreg(1:10, 1:10)
+    2-element Vector{Float64}:
+     -1.19542133983862e-15
+      1.0
+
+    julia> isapprox(a, 0, atol = 1e-12)
+    true
+
+    julia> isapprox(b, 1, atol = 1e-12)
+    true
+    ```
     """
-    linreg(x::AbstractVector, y::AbstractVector) = hcat(fill!(similar(x), 1), x) \ y
+    function linreg(x::AbstractVector{T}, y::AbstractVector{<:Number}) where {T<:Number}
+        A = similar(x, length(x), 2)
+        A[:,1] .= one(T)
+        A[:,2] .= x
+        return A\y
+    end
     export linreg
 
 
